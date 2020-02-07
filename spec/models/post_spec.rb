5aa905d7d9cc38fb167412c73d7f54fc64972586
shadowfox3735 <normanpan180@gmail.com>
2020-02-07 00:52:41 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,9 +6,9 @@ describe Post do
     description: "The post description")
     expect(post).to be_valid
   end
-end
 
-it 'has a summary' do
-  post = Post.create!(title: "My title", description: "The post description")
-  expect(post.post_summary).to eq("My title - The post description")
+  it 'has a summary' do
+    post = Post.create!(title: "My title", description: "The post description")
+    expect(post.post_summary).to eq("My title - The post description")
+  end
 end
